6308a8ae17e30f2372d79202c733cfc340592f78
Xin Ren <iamshrek@126.com>
2017-04-09 17:50:37 -0700

    more comment

--- raspberry_pi_trigger.py
+++ raspberry_pi_trigger.py
@@ -2,7 +2,9 @@ from gpiozero import LED, Button
 from signal import pause
 import requests
 
-
+'''
+	@note: this module is run on raspberry pi computer python-2 terminal, NOT on ec2 server
+'''
 def pressedClose(button):
 
     req = requests.post("http://34.209.46.58:5000/update?udid=d1&status=closed")
@@ -18,14 +20,14 @@ def pressedOpen(button):
     
     print(str(button.pin.number) + "won the game")
 
-#red close
+#red button close
 led = LED(17)
 button = Button(2)
 
 button.when_pressed = pressedClose
 button.when_released = led.off
 
-#blue open
+#blue button open
 led1 = LED(27)
 button1 = Button(3)
 
